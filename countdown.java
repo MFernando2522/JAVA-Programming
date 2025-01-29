80c8e31f4e62068fc6f193a2529f033dc9f77d54
MFernando2522 <166775031+MFernando2522@users.noreply.github.com>
2025-01-29 09:56:17 -0500

    Update countdown.java

--- countdown.java
+++ countdown.java
@@ -1,16 +1,22 @@
 import java.util.Scanner; // import the Scanner class 
 
-class countdown {
-  public static void main(String[] args) {
+public class Main {
+   static void myMethod() {
+       
     Scanner myObj = new Scanner(System.in);
     Boolean flag=true;
-
-
+    
     int numberGiven;
     // Enter username and press Enter
-    System.out.print("Enter number: "); 
+    System.out.print("Enter number: If Not A Number, It Will Fail. "); 
     numberGiven = myObj.nextInt();
 
+    while (myObj.hasNext()) {
+      if (myObj.hasNextInt()) {
+        System.out.println(myObj.nextInt());
+      } else {
+        myObj.next();
+      }
     //counters
     int totalNumber = numberGiven;
     int countingDown = totalNumber;
@@ -19,9 +25,13 @@ class countdown {
         countingDown -=1;
         totalNumber += 1;
         System.out.println(countingDown);
-      }  
+    }}
+    }
+   }
+    public static void main(String[] args) {
+    myMethod();
+    }
+}
 
-    
-  }
-}
-}
+
+
