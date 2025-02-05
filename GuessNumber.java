4c96afd7b8fd0d46d3611b44ce674b6ab3c3a134
MFernando2522 <166775031+MFernando2522@users.noreply.github.com>
2025-02-05 09:35:20 -0500

    Update GuessNumber.java

--- GuessNumber.java
+++ GuessNumber.java
@@ -5,59 +5,36 @@ import java.util.Scanner;
 class GuessNumber {
     public static void main(String[] args) {
 
-        
+        //Scanner name here for scanning the user input
         Scanner userInputSlay = new Scanner(System.in);
 
         int number = 1 + (int)(100* Math.random());
 
-
         //Selfff checker for syntax yuhr coding thing ifgnoire this
         //System.out.print(number);
-
+        System.out.println("Guess a magic number between 0 and 100");
 
         int guess;
         do {
             // Prompt the user to start guessing
-            System.out.println(
-                "Guess the number:");
+            System.out.print("Enter your guess: ");
  
             // Take input for guessing
             guess = userInputSlay.nextInt();
  
             // If the number is guessed
             if (number == guess) {
-                System.out.println(
-                    "Congratulations!"
-                    + " You guessed the number.");
+                System.out.println("Yes, the number is " + number);
                 break; //For exit looping breakkkkKKKKKKKKKKKKK
             }
             else if (number > guess) {
-                System.out.println(
-                    "The number is "
-                    + "greater than " + guess);  //HINTTT
+                System.out.println("Your guess is too low");  //HINTTT
             }
             else if (number < guess) {
-                System.out.println(
-                    "The number is"
-                    + " less than " + guess); //AOTEHRrerRE HINTTTT
+                System.out.println("Your guess is too high"); //AOTEHRrerRE HINTTTT
             }
         }
-        while(true);
-
-
-// WHILE (true) DO
-//     Ask for a guess
-//     Get user input
-
-
-//         Exit the loop
-//     ELSE IF the guess is too high THEN
-//         Give a hint
-//     ELSE
-//         Give a different hint
-//     END IF
-// END WHILE
-
-// END
+        while(true);  //sudo code said to do this. I do break i swear it doesnt always run guys
+userInputSlay.close();  //CLOSE FOR UHHH MINOR ERROR IN CODE WHERE IT SAYS "scanner never closed" nerd emoji this guy oh my god
     }
 }
